b4a9cfa202d4d3342581ec57b91addc6d1803128
Naman Arora <nmnarora600@gmail.com>
2024-04-12 13:24:47 +0530

    meta data picture

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
     
       },
       {
-        url: "/logo.png", 
+        url: "/metalogo.jpeg", 
       
       },
     ],
--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes:['/','/favicon.ico','/file.png','/f/(.*)','/api/(.*)','/audio.png','/download.jpg','/error.jpg','/filesend.jpg','/image.png','/logo.png','/notfound.jpg','/pdf.png','/video.png','/video2.png']
+    publicRoutes:['/','/favicon.ico','/file.png','/f/(.*)','/api/(.*)','/audio.png','/download.jpg','/error.jpg','/filesend.jpg','/image.png','/logo.png','/notfound.jpg','/pdf.png','/video.png','/video2.png','/metalogo.jpeg']
 });
 
 export const config = {
