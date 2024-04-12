ce74e67e0343267fddce1ee9f6534a39ed60ec35
Naman Arora <nmnarora600@gmail.com>
2024-04-12 12:48:09 +0530

    removed alert

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
-import Image from "next/image";
+
 import Header from "./_components/Header";
 import Hero from "./_components/Hero";
-import Alert from "./(dashboard)/(routes)/upload/_components/Alert";
+
 
 export default function Home() {
 
